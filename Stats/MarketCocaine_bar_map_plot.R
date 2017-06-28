b4eb88daa011ffa3f3b92c0b51c7dd727e831a79
SimonDele <simonpauldelecourt@gmail.com>
2017-06-28 15:51:23 +0100

    plot

--- Stats/MarketCocaine_bar_map_plot.R
+++ Stats/MarketCocaine_bar_map_plot.R
@@ -1,21 +1,17 @@
 #data <- as.data.frame(read.csv("data.csv"))
 
-#install.packages("stringr")
-#install.packages("units")
-
 library(stringr)
-
 library(ggmap)
 
 #library(rworldmap)
 
-#matching_vector <- c( (str_detect(data$category, "Cocaine")| str_detect(data$title, "cocaine") | str_detect(data$brief, "cocaine") ) & !str_detect(data$origin, "Worldwide"))
 matching_vector <- c( (str_detect(data$category, "Drugs") ) & !str_detect(data$origin, "Worldwide"))
 
 sumup <- sort(summary(data[matching_vector, "origin"]), decreasing=TRUE)
 
-barplot(sumup[1:10], main="Cocaine market in the world", xlab="Countries", ylab="Number of ads")
 
+barp <- barplot(sumup[1:10], main="Number of ads of Drugs in the World", xlab="Countries", ylab="Number of ads",ylim = c(0,3300),  col = rainbow(10), cex.names = 0.8)
+barp <- text(x = barp, y = sumup[1:10], label = sumup[1:10], pos=3 , cex = 0.8, col= "red")
 
 
 data_country <- read.csv("lat_long.csv")
@@ -25,20 +21,9 @@ data_plot <- merge(v, lat_long,  by.x = "name", by.y = "Country" )
 
 
 
-
-#world <- map_data("world")
-#worldMap <- ggplot(world, aes(x=long, y=lat, size = sqrt(data_plot$frequency) ,data = data_plot)) +
-#  geom_path() +
-#  scale_y_continuous(breaks=(-2:2) * 30) +
-#  scale_x_continuous(breaks=(-4:4) * 45)
-
-#worldPoints <- worldMap +  geom_point(aes())
-
-#worldPoints
-
 #EUROPE
 map <- get_map(location = 'Europe', zoom =4 )
-mapPoints <- ggmap(map)  + xlab("") + ylab("") + title("edf")+ ggtitle("Number of ads of Drugs in the World")+
+mapPoints <- ggmap(map)  + xlab("") + ylab("") + ggtitle("Number of ads of Drugs in the World")+
   geom_point(data = data_plot,aes(x =long, y = lat, size =frequency)) +scale_size_continuous(limits=c(0,3000),breaks=c(0,500,1000,1500,2000), range = c(2,20)) 
 
 mapPoints
