9f2d5dc98a24e3d6bf7b3d3bc91308436a6260b9
SimonDele <simonpauldelecourt@gmail.com>
2017-06-29 10:28:38 +0100

    distrib per category

--- Stats/BlackMarket.R
+++ Stats/BlackMarket.R
@@ -8,6 +8,7 @@ category1 <- str_match(data$category, regex )
 categorysort <- sort(table(category1[,2]), decreasing=TRUE)
 
 
+#Useless here but I was thinking of doing a stacked plot 
 mylist <-list()
 for(i in 1:length(categorysort)){
   matching_vector <- c(str_detect(category1[,2], names(categorysort)[i])); #spotting the lines corresponding to the i-th category
@@ -20,5 +21,32 @@ for(i in 1:length(categorysort)){
 
 
 
-barplot(categorysort[1:10], main="Black Market", xlab="Category", ylab="Number of ads")
+#barplot(categorysort[1:10], main="Black Market", xlab="Category", ylab="Number of ads")
+
+categorysort <- categorysort[1:10]
+
+# Calculation in percentage
+piepercent<- round(100*categorysort/sum(categorysort), 1)
+# round(a,1) : one digit after the comma
+
+
+# 1- Labels :
+lab <- c()
+
+for(i in 1:length(piepercent)) {
+  lab[i] <- paste(piepercent[[i]], "%", sep=" ")
+}
+
+# 2- Title :
+title <- "Distribution of ads per category"
+
+# 3- Colors :
+c <- rainbow(length(piepercent))
+
+# 4- Plot :
+pie(piepercent,labels = lab, main = title ,col=c)
+
+# 5- Legend :
+legend(1.2,0.9, names(piepercent), cex = 0.8, fill = c)
+
 
