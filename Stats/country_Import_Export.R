0fdf859d1daf3d465e5f48c3dbcad8dbc320e417
DONZÉ Edouard <edouard.donze@insa-rouen.fr>
2017-07-03 18:33:38 +0100

    report

--- Stats/country_Import_Export.R
+++ Stats/country_Import_Export.R
@@ -1,125 +1,127 @@
 
-  #####################################################################
-  #                            Library
-  #####################################################################
-  
-  library (stringr)
-  library(plotrix)
-  
-  #####################################################################
-  #                         Initialization
-  #####################################################################
-  
-  #data <- as.data.frame(read.csv("data.csv"))[2:21]
-  
-  country <- "France"
-  num <- 0
-  
-  # Importation / Exportation :
-  if (num == 0) { 
-    way <- "origin"
-    txt <- "- Exportation"
-  } else if (num == 1) {
-    way <- "destination"
-    txt <- "- Importation"
-  }
-  
-  #####################################################################
-  #                           Analysis
-  #####################################################################
-  
-  # Country as destination
-  matching_vector <- str_detect(data[,way], country)
-  
-  # list of the categories (among the line that have "Country" as origin)
-  # -> Products (categories) exporting by the country 
-  country_cat <- data[matching_vector,"category"] 
-  
-  # Handling of this categories
-  # Regular expression for spliting the categories
-  regex <- "/(.*)/(.*)/(.*)"
-  cat <- str_match(country_cat, regex)
-  
-  # Counting this categories 
-  tab <- table(cat[,3])   #cat[,3] : 2nd category 
-  tab <- sort(tab, decreasing = TRUE)  # Sorting (biggest in first) 
-  tab <- tab[1:10] # Taking only the most important
-  
-  #############
-
-  # Country as destination
-  matching_vector2 <- str_detect(data[,"destination"], country)
-  
-  # list of the categories (among the line that have "Country" as origin)
-  # -> Products (categories) exporting by the country 
-  country_cat2 <- data[matching_vector2,"category"] 
-  
-  # Handling of this categories
-  # Regular expression for spliting the categories
-  regex <- "/(.*)/(.*)/(.*)"
-  cat2 <- str_match(country_cat2, regex)
-  
-  # Counting this categories 
-  tab2 <- table(cat2[,3])   #cat[,3] : 2nd category 
-  tab2 <- sort(tab2, decreasing = TRUE)  # Sorting (biggest in first) 
-  tab2 <- tab2[1:10] # Taking only the most important
-  
-  tab <- data.frame(tab)
-  tab2 <- data.frame(tab2)
-  
-  v <- merge(tab,tab2,by.x="Var1",by.y="Var1",all = TRUE)
-  
-  
-  #####################################################################
-  #                           Pie Chart     
-  #####################################################################
-  
-  #par(mfrow = c(1,2))
-  par(mar=c(5.1, 4.1, 4.1, 15.1), xpd=TRUE)
-  
-  
-  # 2- Title :
-  title <- "exp"
-  
-  # 3- Colors :
-  c <- rainbow(length())
   
   # 4- Plot :
-  pie(v[1:10,2],labels=v[,1], main = title ,col=c)
-  
-  
-  ####
-  
-  # 2- Title :
-  title <- "imp"
-  
-  # 3- Colors :
-  c <- rainbow(length(tab2))
-  
-  # 4- Plot :
-<<<<<<< HEAD
-  pie(piepercent,labels = lab, main = title ,col=c)
-  
-  # 5- Legend :
-  legend("topright", legend = (piepercent), cex = 0.8, fill = c ,  inset=c(-2,-5))
-  
-}
+  #pie(v[1:10,2],labels=v[,1], main = title ,col=c)
 
-
-#####################################################################
-#                           Function  
-#####################################################################
-
-# 1st argument : Country
-# 2nd : 
-#   0 -> Exportation
-#   1 -> Importation
-
-
-#par(mfrow = c(1,2))
-
-#country_Import_Export("United Kingdom",0)
-country_Import_Export("United Kingdom",0)
-=======
-  pie(tab2, main = title ,col=c)
->>>>>>> dfb269def6a0a231a5977d2764c43b4cd0d95781
+#data <- as.data.frame(read.csv("data.csv"))
+  
+  #-----------------------------------------------
+  #   Importation / Exportation of a country
+  #-----------------------------------------------
+  
+    #-------------------
+    #  Initialization
+    #-------------------
+    
+    country <- "France"
+    
+    #---------------------------
+    #    Analysis - Exportation
+    #---------------------------
+    
+    # Country as destination
+    matching_vector_exp <- str_detect(data[,"origin"], country)
+    
+    # list of the categories (among the line that have "Country" as origin)
+    # -> Products (categories) exporting by the country 
+    country_cat_exp <- data[matching_vector_exp,"category"] 
+    
+    # Handling of this categories
+    # Regular expression for spliting the categories
+    regex <- "/(.*)/(.*)/(.*)"
+    cat_exp <- str_match(country_cat_exp, regex)
+    
+    # Counting this categories 
+    tab_exp <- table(cat_exp[,3])   #cat[,3] : 2nd category 
+    tab_exp <- sort(tab_exp, decreasing = TRUE)  # Sorting (biggest in first) 
+    tab_exp <- tab_exp[1:10] # Taking only the most important
+    
+    #---------------------------
+    #    Analysis - Importation
+    #---------------------------
+    
+    # Country as destination
+    matching_vector_imp <- str_detect(data[,"destination"], country)
+    
+    # list of the categories (among the line that have "Country" as origin)
+    # -> Products (categories) exporting by the country 
+    country_cat_imp <- data[matching_vector_imp,"category"] 
+    
+    # Handling of this categories
+    # Regular expression for spliting the categories
+    regex <- "/(.*)/(.*)/(.*)"
+    cat_imp <- str_match(country_cat_imp, regex)
+    
+    # Counting this categories 
+    tab_imp <- table(cat_imp[,3])   #cat[,3] : 2nd category 
+    tab_imp <- sort(tab_imp, decreasing = TRUE)  # Sorting (biggest in first) 
+    tab_imp <- tab_imp[1:10] # Taking only the most important
+    
+    #-------------------------
+    #    Analysis - Fusion
+    #-------------------------
+    
+    tab_exp <- data.frame(tab_exp)
+    tab_imp <- data.frame(tab_imp)
+    
+    tab <- merge(tab_exp,tab_imp,by.x="Var1",by.y="Var1",all = TRUE)
+    
+    for(i in 1:length(tab[,2])){
+      if(is.na(tab[i,2])) {tab[i,2] <-0}
+    }
+    
+    for(i in 1:length(tab[,3])){
+      if(is.na(tab[i,3])) {tab[i,3] <-0}
+    }
+    
+    #---------------------------
+    #    Pie Chart - Exporation     
+    #---------------------------
+    
+    par(mfrow = c(1,2))
+    
+    # 1- Labels :
+    
+    # Calculation in percentage
+    piepercent_exp <- round(100*tab[,2]/sum(tab[,2]), 1)
+    # round(a,1) : one digit after the comma
+    
+    lab_exp <- c()
+    
+    for(i in 1:length(piepercent_exp)) {
+      lab_exp[i] <- paste(piepercent_exp[[i]], "%", sep=" ")
+    }
+    
+    # 2- Title :
+    title <- paste(country, "- Exportation", sep=" ")
+    
+    # 2- Colors :
+    c <- rainbow(length(tab[,1]))
+    
+    # 3- Plot :
+    pie(tab[1:10,2],labels=lab_exp, main = title ,col=c)
+    
+    #---------------------------
+    #    Pie Chart - Importation     
+    #---------------------------
+    
+    
+    # 1- Labels :
+    
+    # Calculation in percentage
+    piepercent_imp <- round(100*tab[,3]/sum(tab[,3]), 1)
+    # round(a,1) : one digit after the comma
+    
+    lab_imp <- c()
+    
+    for(i in 1:length(piepercent_imp)) {
+      lab_imp[i] <- paste(piepercent_imp[[i]], "%", sep=" ")
+    }
+    
+    # 2- Title :
+    title <- paste(country, "- Importation", sep=" ")
+    
+    # 3- Plot :
+    pie(tab[,3],labels=lab_imp, main = title ,col=c)
+    
+    legend(x=-5.5,y=-1,tab[,1], cex = 0.8, fill = c,ncol=4,border=NA, xpd=NA)
