42612341545f90a776a1e58eeef327200075715d
SimonDele <simonpauldelecourt@gmail.com>
2017-07-03 17:47:56 +0100

    price of drugs

--- Stats/NbAdsWorld.R
+++ Stats/NbAdsWorld.R
@@ -9,8 +9,11 @@ library(stringr)
 matching_vector <- c(  !str_detect(data$origin, "Worldwide") & !str_detect(data$origin, "NULL"))
 
 sumup <- sort(summary(data[matching_vector, "origin"]), decreasing=TRUE)
-
+par(mfrow = c(1,1))
 #Bar plot with the total number ofs ads in each country
-par(mar=c(5,2,4,0)+.1)
-barp <- barplot(sumup[1:10], main="Number of ads in the World", xlab="Countries", ylab="Number of ads",ylim = c(0,4000),  col = rainbow(10), cex.names = 0.8)
-barp <- text(x = barp, y = sumup[1:10], label = sumup[1:10], pos=3 , cex = 0.8, col= "black")
+#par(mar=c(5,2,4,0)+.1)
+par(las=1)
+
+barp <- barplot(sumup[1:10], main="Number of ads in the World", xlim= c(0,max(sumup[1:10])+500), xlab="Number of ads",horiz = TRUE,  col = rainbow(10), cex.names = 0.8, angle = 45)
+barp <- text(y = barp, x = sumup[1:10], label = sumup[1:10], pos=4 , cex = 0.8, col= "black")
+
--- Stats/PriceOfDrugs.R
+++ Stats/PriceOfDrugs.R
@@ -1,4 +1,4 @@
-#data <- as.data.frame(read.csv("data.csv"))
+data <- as.data.frame(read.csv("data.csv"))
 library(stringr)
 
 selectDrug <- function(drugName){
@@ -21,4 +21,43 @@ priceDrugs$med <- round(priceDrugs$med,2)
 priceDrugs <- priceDrugs[order(priceDrugs$med, decreasing=TRUE), ]
 
 barp <- barplot(priceDrugs$med, main="Average Price of Drugs in the World", names.arg = priceDrugs$drugs, ylim = c(0,500),  xlab="Drugs", ylab="Price in USD", cex.names = 0.8, col =rainbow(length(priceDrugs$drugs)) )
-barp <- text(x = barp, y = priceDrugs$med, label = paste(priceDrugs$med, " $", sep=""), pos=3 , cex = 0.8, col= "red")
+barp <- text(x = barp, y = priceDrugs$med, label = paste(priceDrugs$med, " $", sep=""), pos=3 , cex = 0.8, col= "black")
+
+
+#####################################
+#        Prices find on articles 
+#####################################
+
+cols <- c("Cocaine", "Meth", "Opioids",      "Cannabis"      , "Steroids", "Ecstasy", "Ketamine", "Heroin",  "NBOME","Shrooms", "Tobacco", "Benzos", "Paraphernalia" , "MDMA", "Amphetamine", "Crack", "LSD"   , "URL")
+
+ref1  <- c(    35   ,   200  ,     NA   ,     (5.3 + 7.85)/2   ,    NA     ,     15   ,     25    ,    100   ,     NA  ,    NA   ,    NA    ,     NA  ,        NA      ,    40 ,   5         ,  NA    ,  NA     ,   "http://www.drugwise.org.uk/how-much-do-drugs-cost/")
+ref2  <- c(    67   ,   NA   ,     NA   ,      51              ,    NA     ,     15   ,     32    ,    129   ,     NA  ,    NA   ,    NA    ,     NA  ,        NA      ,   51  ,   15        ,  97    ,  NA     , "http://www.telegraph.co.uk/news/uknews/crime/11346133/The-cost-of-street-drugs-in-Britain.html")
+ref3  <- c(    110  ,   80   ,     NA   ,      NA              ,    NA     ,     NA   ,     NA    ,    170   ,     NA  ,    5.7  ,    NA    ,     NA  ,        NA      ,  150  ,   NA        ,   NA   ,  12000  ,   "http://www.rehabcenter.net/the-average-cost-of-illegal-drugs-on-the-street/ " )
+ref4  <- c(    80   ,   109  ,     NA   ,      NA              ,    NA     ,   19.12  ,     NA    ,    91.16 ,     NA  ,    NA   ,    NA    ,     NA  ,        NA      ,    NA ,   NA        ,   NA   ,     NA  ,   " http://o.canada.com/business/interactive-what-illegal-drugs-cost-on-the-street-around-the-world")
+ref5  <- c(     64  ,   NA   ,     NA   ,      NA              ,    NA     ,     20   ,     NA    ,    NA    ,     NA  ,    NA   ,    NA    ,     NA  ,        NA      ,    NA ,   NA        ,  NA    ,   NA    , " http://www.thestudentpocketguide.com/2012/01/student-life/health-and-relationships/facts-about-drugs/")
+
+doc_drugs <- t(data.frame(ref1, ref2, ref3, ref4, ref5))
+colnames(doc_drugs) <- cols
+
+
+price_doc <- c()
+for(i in 1 : length(cols)){
+  price_doc[i] <- summary(as.numeric(doc_drugs[,i]))[[4]]
+}
+price_doc.data <- data.frame(cols, price_doc)
+
+beside_plot <- merge(price_doc.data, priceDrugs, by.x ="cols", by.y ="drugs") 
+
+rownames(beside_plot) <- beside_plot[,1]
+beside_plot <- beside_plot[,-1]
+
+par(las=1)#display yaxis horizontally
+par(mar=c(8,8,4,1)) #give space for yaxis
+
+barplot(rbind(beside_plot[,1], beside_plot[,2]), main="Average price of Drugs",
+        xlab="Price of Drugs", beside=TRUE, names.arg = rownames(beside_plot), col=1:2, horiz = TRUE, space = c(0,0.2))
+
+lab <- c("Street", "AlphaBay")
+
+legend("topright",lab,fill=1:2)
+
