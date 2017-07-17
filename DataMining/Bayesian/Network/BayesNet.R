57427958e02571ced3b0e7625aae5e1dedbbd627
SimonDele <simonpauldelecourt@gmail.com>
2017-07-17 12:08:37 +0100

    expectancy bayesNet

--- DataMining/Bayesian/Network/BayesNet.R
+++ DataMining/Bayesian/Network/BayesNet.R
@@ -42,12 +42,16 @@ bayesian.data$timestamp <-  as.Date(bayesian.data$timestamp)
 bayesian.data$timestamp <- bayesian.data$timestamp - bayesian.data$sold_since
 bayesian.data$timestamp <- as.numeric(bayesian.data$timestamp)
 
+# 1 day on the market at least
+bayesian.data <- bayesian.data[which(bayesian.data$timestamp > 0),]
+
 #Calculate profitability
-bayesian.data$products_sold <- bayesian.data$products_sold / bayesian.data$timestamp * 30
+bayesian.data$products_sold <- (bayesian.data$products_sold / bayesian.data$timestamp) * 30
 names(bayesian.data)[match("products_sold",names(bayesian.data))] <- "profitability"
 
 #Discretize profitability
-bayesian.data$profitability <- arules::discretize(bayesian.data$profitability, method="frequency", categories = 5)
+nbCategory <- 5
+bayesian.data$profitability <- arules::discretize(bayesian.data$profitability, method="frequency", categories = nbCategory)
 
 bayesian.data <- subset(bayesian.data, select= -c(sold_since, timestamp))
 
@@ -63,9 +67,34 @@ bayesian.data <- bayesian.data[!is.element(bayesian.data$profitability, NA),]
 #   Bayesian Network
 #---------------------
 
+#Neural Network
 res <- hc(bayesian.data)
 plot(res)
 
+#Conditionnal probability
 fittedbn <- bn.fit(res, data = bayesian.data)
-
 print(fittedbn$profitability)
+
+#Handling interval
+interv <- levels(bayesian.data$profitability)
+interv <- unlist(strsplit(interv, ","))
+interv <- gsub(pattern = "[^0-9.]*", replacement = "", interv)
+interval <- data.frame(interv[seq(1, length(interv), 2)],interv[seq(2, length(interv), 2)])
+colnames(interval) <- c("left", "right")
+interval$left <- as.numeric(levels(interval$left))
+interval$right <- as.numeric(levels(interval$right))
+
+expectancy <- c()
+#Calculate 
+for(i in 0:length(table(bayesian.data$category))-1){
+  left <- 0
+  right <- 0
+  for(j in 1:nbCategory){
+    left<-left + interval$left[j] * fittedbn$profitability$prob[i*nbCategory + j]
+    right<-right + interval$right[j] * fittedbn$profitability$prob[i*nbCategory + j]
+  }
+  expectancy[i] <- paste("[", left , "," , right , "]")
+  
+}
+
+
