6454546e44edec2e9372b468fbce4e12ab52d9fd
DONZÉ Edouard <edouard.donze@insa-rouen.fr>
2017-07-27 17:18:35 +0100

    Evo

--- More stats/Evolution.R
+++ More stats/Evolution.R
@@ -8,6 +8,7 @@ library(stringr)
 library(lubridate)
 library(dygraphs)
 library(xts)
+library(zoo)
 
 #-----------------
 #   New Data 
@@ -26,28 +27,18 @@ Evo.data$sold_since <-  as.Date(Evo.data$sold_since)
 # Remove ads from 2014 (only 10)
 Evo.data <- Evo.data[which(year(Evo.data$sold_since) > 2014),]
 
+# Month
+Evo.data$sold_since <- as.yearmon(Evo.data$sold_since,"%Y-%m-%d")
+
+
 tab_Evo <- table(Evo.data$sold_since)
 evo.data <- as.data.frame(tab_Evo)
-evo.data$Var1 <- as.Date(evo.data$Var1)
-evo.data <- as.xts(evo.data)
+
+evo.data$Var1 <- as.yearmon(evo.data$Var1,"%b %Y")
+
 
 xts2 <- xts(x=evo.data$Freq, order.by=evo.data$Var1)
-
-plot(tab_Evo)
-
-a <- c()
-
-for(i in 1:5) {
-  a[i] <- xts(tab_Evo[i])
-}
-
-data <- rnorm(5)
-dates <- seq(as.Date("2017-05-01"), length=5, by="days")
-xts2 <- xts(x=data, order.by=dates)
-
-
-
-dygraph(xts2)
+dygraph(xts2) %>% dyRangeSelector()
 
 #ad2015 <- nrow(Evo.data[which(year(Evo.data$sold_since) == 2015),])
 #ad2016 <- nrow(Evo.data[which(year(Evo.data$sold_since) == 2016),])
