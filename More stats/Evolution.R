f07692636c456c490603ccc4ea2e57eafdd0e6c2
DONZÉ Edouard <edouard.donze@insa-rouen.fr>
2017-07-28 16:39:44 +0100

    Evo

--- More stats/Evolution.R
+++ More stats/Evolution.R
@@ -9,7 +9,6 @@ library(lubridate)
 library(dygraphs)
 library(xts)
 library(zoo)
-library(stats)
 library(forecast)
 
 
@@ -18,46 +17,52 @@ library(forecast)
 #-----------------
 
 # Select the column of the data that are interesting 
-evo.data <- subset(data, select=c(category,sold_since))
+Evo.data <- subset(data, select=c(category,sold_since))
   # Subset : choose the colunm that you want
 
 # Remove ads with no informations
-evo.data <- evo.data[which(evo.data$sold_since != "NULL"),]
+Evo.data <- Evo.data[which(Evo.data$sold_since != "NULL"),]
 
 # Formatting
-evo.data$sold_since <-  as.Date(evo.data$sold_since)
+Evo.data$sold_since <-  as.Date(Evo.data$sold_since)
 
 # Remove ads from 2014 (only 10)
-evo.data <- evo.data[which(year(evo.data$sold_since) > 2014),]
+Evo.data <- Evo.data[which(year(Evo.data$sold_since) > 2014),]
 
 # Month
-evo.data$sold_since <- as.yearmon(evo.data$sold_since)
+Evo.data$sold_since <- as.yearmon(Evo.data$sold_since)
 
 # Calculating the number od ads per Month
-tab_evo <- table(evo.data$sold_since)
-Evo.data <- as.data.frame(tab_evo)
+tab_Evo <- table(Evo.data$sold_since)
+Evo.data <- as.data.frame(tab_Evo)
 colnames(Evo.data) <- c("Time","Ads")
-
-# Formatting
+Evo.data <- Evo.data[1:(length(Evo.data$Time)-1),]
 Evo.data$Time <- as.yearmon(Evo.data$Time)
 
-# Time element
-xts.data <- as.xts(x=Evo.data$Ads, order.by=Evo.data$Time)
-colnames(xts.data) <- "Number of Ads"
+# Time series object :
+# start: from the first date that we have 
+# frequency=12 i.e. monthly observations ((1=annual, 4=quartly)
+Evo <- ts(Evo.data$Ads, start=Evo.data$Time[1], frequency=12)
 
+# Prediction until Dec 2017 :
+  # Analyse 
+fit <- HoltWinters(Evo)
+  # Prediction 
+  # n.ahead : predict next six month with a confidence of 0.95
+  # Prediction.interval : up and low prediction
+pred <- predict(fit, n.ahead=6,prediction.interval=TRUE,level=0.95)
 
-dygraph(xts.data,  main = "Evolution of the Market", ylab = "Number of Ads") %>%
-  dyOptions(stackedGraph=TRUE, drawPoints=TRUE, pointSize=2) %>%
+# Merge 
+all <- cbind(Evo,pred)
+
+# Ploting
+dygraph(all, main = "Evolution of the Market", ylab = "Number of Ads") %>%
+  dySeries("Evo", label = "Number of Ads") %>%
+  dySeries(c("pred.lwr", "pred.fit", "pred.upr"), label = "Prediction") %>%
+  dyOptions(drawPoints=TRUE, pointSize=2) %>%
   dyHighlight(highlightCircleSize=6) %>%
   dyEvent("2017-06-01", "Shut-Down", labelLoc = "bottom") %>%
   dyRangeSelector
-
-
-predicted <- predict(xts.data,5)
-all <- cbind(xts.data,predicted)
-
-dygraph(predicted) %>%
-  dySeries(c("lwr", "fit", "upr"), label = "Deaths")
 
 
 #ad2015 <- nrow(Evo.data[which(year(Evo.data$sold_since) == 2015),])
