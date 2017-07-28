d33a73d9ff2e658b759881a99f2473a2ed861c1b
DONZÉ Edouard <edouard.donze@insa-rouen.fr>
2017-07-28 12:27:18 +0100

    Evo

--- More stats/Evolution.R
+++ More stats/Evolution.R
@@ -9,36 +9,56 @@ library(lubridate)
 library(dygraphs)
 library(xts)
 library(zoo)
+library(stats)
+library(forecast)
+
 
 #-----------------
 #   New Data 
 #-----------------
 
 # Select the column of the data that are interesting 
-Evo.data <- subset(data, select=c(category,sold_since))
+evo.data <- subset(data, select=c(category,sold_since))
   # Subset : choose the colunm that you want
 
 # Remove ads with no informations
-Evo.data <- Evo.data[which(Evo.data$sold_since != "NULL"),]
+evo.data <- evo.data[which(evo.data$sold_since != "NULL"),]
 
 # Formatting
-Evo.data$sold_since <-  as.Date(Evo.data$sold_since)
+evo.data$sold_since <-  as.Date(evo.data$sold_since)
 
 # Remove ads from 2014 (only 10)
-Evo.data <- Evo.data[which(year(Evo.data$sold_since) > 2014),]
+evo.data <- evo.data[which(year(evo.data$sold_since) > 2014),]
 
 # Month
-Evo.data$sold_since <- as.yearmon(Evo.data$sold_since,"%Y-%m-%d")
+evo.data$sold_since <- as.yearmon(evo.data$sold_since)
+
+# Calculating the number od ads per Month
+tab_evo <- table(evo.data$sold_since)
+Evo.data <- as.data.frame(tab_evo)
+colnames(Evo.data) <- c("Time","Ads")
+
+# Formatting
+Evo.data$Time <- as.yearmon(Evo.data$Time)
+
+# Time element
+xts.data <- as.xts(x=Evo.data$Ads, order.by=Evo.data$Time)
+colnames(xts.data) <- "Number of Ads"
 
 
-tab_Evo <- table(Evo.data$sold_since)
-evo.data <- as.data.frame(tab_Evo)
+dygraph(xts.data,  main = "Evolution of the Market", ylab = "Number of Ads") %>%
+  dyOptions(stackedGraph=TRUE, drawPoints=TRUE, pointSize=2) %>%
+  dyHighlight(highlightCircleSize=6) %>%
+  dyEvent("2017-06-01", "Shut-Down", labelLoc = "bottom") %>%
+  dyRangeSelector
 
-evo.data$Var1 <- as.yearmon(evo.data$Var1,"%b %Y")
 
+predicted <- predict(xts.data,5)
+all <- cbind(xts.data,predicted)
 
-xts2 <- xts(x=evo.data$Freq, order.by=evo.data$Var1)
-dygraph(xts2) %>% dyRangeSelector()
+dygraph(predicted) %>%
+  dySeries(c("lwr", "fit", "upr"), label = "Deaths")
+
 
 #ad2015 <- nrow(Evo.data[which(year(Evo.data$sold_since) == 2015),])
 #ad2016 <- nrow(Evo.data[which(year(Evo.data$sold_since) == 2016),])
