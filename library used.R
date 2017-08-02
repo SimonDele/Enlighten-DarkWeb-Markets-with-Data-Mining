6c0e6976fd669c5503e1ce7a973c60e512e11f38
SimonDele <simonpauldelecourt@gmail.com>
2017-08-02 15:53:51 +0100

    library

---
+++ library used.R
@@ -0,0 +1,60 @@
+#----------------------------------------------------------
+#                       Library :
+#----------------------------------------------------------
+
+#install.packages("stringr")
+#install.packages("units")
+#install.packages("ggmap")
+#install.packages("plotrix")
+#install.packages("rattle")
+#install.packages("rpart")
+#install.packages("rpart.plot")
+#install.packages("RColorBrewer")
+#install.packages("arules")
+#install.packages("arulesViz")
+#install.packages("e1071")
+#install.packages("bnlearn")
+#install.packages("lubridate")
+#install.packages("dygraphs")
+#install.packages("zoo")
+#install.packages("forecast")
+#install.packages("RTextTools")
+#install.packages("tm")
+
+# Mamipulation string
+library(stringr)
+
+# Using unit
+library(units)
+
+# Mamipulation Date
+library(lubridate)
+library(zoo)
+library(forecast)
+
+# dynamic Plot
+library(dygraphs)
+
+# Plot a map
+library(ggmap)
+library(plotrix)
+
+# Decision Tree
+library(rattle)
+library(rpart)
+library(rpart.plot)
+library(RColorBrewer)
+
+# Association rules
+library(arules)
+library(arulesViz)
+
+# Bayesian classification, naive algorithm
+library(e1071)
+
+# Bayesian Network
+library(bnlearn)
+
+#Text mining 
+library(RTextTools)
+library(tm)
